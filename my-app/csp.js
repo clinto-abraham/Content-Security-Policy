d31fd1ccef46d9fd7f93c19e3f24cd9eaf897322
clinto92 <clinto92@gmail.com>
2021-08-26 21:31:01 +0530

    csp.js file configured for csp directives

--- my-app/csp.js
+++ my-app/csp.js
@@ -15,6 +15,29 @@ module.exports = {
     "connect-src": [
       "'self'",
       "https://mybackend.com"
+    ],
+    "script-src": [
+        "'self'",
+        "https://staging/backend.com"
+    ],
+    "script-src-attr": [ //  Specifies valid sources for JavaScript inline event handlers.
+        "'self'"
+    ], 
+    "style-src-elem": [ // Specifies valid sources for stylesheets <style> elements and <link> elements with rel="stylesheet".
+        "'self'"
+    ],
+    "object-src": [
+      "'none'"
+    ],
+    "img-src": [
+        "'self'",
+        "http://staging/hhs/covid19.com"
     ]
     }
   }
+
+//   https://www.npmjs.com/package/react-csp        - refer package npmjs for react-csp config into node module
+//   https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+//   https://stackoverflow.com/questions/30280370/how-does-content-security-policy-csp-work
+//   https://content-security-policy.com/
+//   https://stackoverflow.com/questions/42922784/what-s-the-purpose-of-the-html-nonce-attribute-for-script-and-style-elements
